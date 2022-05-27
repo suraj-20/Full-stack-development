40337f75a4cec03c7efdb8d484ebdd4c0bb402c2
Suraj <kumarsuraj54057@gmail.com>
2022-05-27 16:02:53 +0530

    JavaScript

---
+++ 3.JavaScript/variables.js
@@ -0,0 +1,20 @@
+
+// How to declare a variables in JS ?
+
+// var means variable.
+
+var num = 10;
+num = 20;
+console.log(num);
+
+// How to declare a const in JS ?
+
+const marks = 100;
+// marks = 200;   (we did not reinisialize the value in const.)
+console.log(marks);
+
+// How to declare a let in JS ?
+
+var myName = "Suraj"
+myName = "Kumar"
+console.log(myName);
