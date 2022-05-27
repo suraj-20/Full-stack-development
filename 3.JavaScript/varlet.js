7159417710a4f3806259b74d265bc1ffe34d7071
Suraj <kumarsuraj54057@gmail.com>
2022-05-27 17:26:04 +0530

    Varlet

--- 3.JavaScript/varlet.js
+++ 3.JavaScript/varlet.js
@@ -3,3 +3,22 @@
 
 // How to declare function in JS ?
 // Syntex --> function<nameOfFunction>(<params>){}
+
+function testDeclaration(){
+    // let - scope will be entire function.
+    if(true){
+        var x = 10;
+        let y = 20;  // scope will be inside if block.
+        console.log("This is inside if block.");
+        console.log(x);  //10
+        console.log(y);  //20
+    }
+    console.log("This is outside if block.")
+    console.log(x);  //10
+    console.log(y);  //y is not defined
+}
+
+// Declaring using var has function lavel scope.
+// Declaring using let has block lavel scope. 
+
+testDeclaration();
