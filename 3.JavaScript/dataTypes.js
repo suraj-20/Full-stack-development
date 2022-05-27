b4448c12a47fff2db200f0ef23f78369f1314120
Suraj <kumarsuraj54057@gmail.com>
2022-05-27 18:48:53 +0530

    DataTypes

---
+++ 3.JavaScript/dataTypes.js
@@ -0,0 +1,42 @@
+
+// Data Types.
+
+// C - int, char, float, double.
+// Java - int, char, float, double, string, long, date, decimal.
+
+// int x = 10;
+// var x = 10;
+// x = "name";
+
+// DataTypes in JS ?
+// 1. number
+// 2. string
+// 3. object
+// 4. function
+// 5. undefined
+
+let x;  // Value is not defined.
+// Operator to find datatypes of an identifiers - "typeof".
+console.log(typeof x);  // undefined
+
+x = 10;
+console.log(typeof x);  // number
+
+x = "name";
+console.log(typeof x);  // string
+
+x = {id:1, name:'mus', dept:"doctor"};
+console.log(typeof x);  // object
+
+x = function print(){
+    console.log("This is a funtion")
+}
+console.log(typeof x);  // function
+
+x = new Date;
+console.log(x); 
+console.log(typeof x);  // object
+console.log(x instanceof Date);  //true
+
+x = null;  // Defined with no data.
+console.log(typeof x);  //object
