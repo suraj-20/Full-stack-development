94a9dcf395a1566694c0028bfa1db8f9fa0c3332
Suraj <kumarsuraj54057@gmail.com>
2022-06-05 14:03:41 +0530

    NodeJS

--- 4.food-delivery-app/index-node.js
+++ 4.food-delivery-app/index-node.js
@@ -6,7 +6,7 @@ const http = require("http");
 
 // Step 2: Create server using http.
 http.createServer((req, res)=>{
-    res.end("Hello world from NodeJS");
-}).listen(4400);
+    res.end("Hello world from NodeJS");  // end the response.
+}).listen(4400);  // the server object listens on port 4400. 
 
 console.log("Server is listening on 4400");
