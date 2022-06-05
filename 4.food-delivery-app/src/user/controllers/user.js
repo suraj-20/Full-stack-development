ed482a8a72d7e7700b64ac1bc48284cdb07998a7
Suraj <kumarsuraj54057@gmail.com>
2022-06-05 15:47:56 +0530

    Node JS

--- 4.food-delivery-app/src/user/controllers/user.js
+++ 4.food-delivery-app/src/user/controllers/user.js
@@ -3,12 +3,12 @@
 module.exports.register = (req, res)=>{
     // read req.body.
     // DB calls.
-    res.write("This is register request");
+    res.end("This is register request");
 }
 
 // api/user/login
 module.exports.login = (req, res)=>{
     // read req.body.
     // DB calls.
-    res.write("This is login request");
+    res.end("This is login request");
 }
