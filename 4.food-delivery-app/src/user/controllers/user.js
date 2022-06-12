8aa4b839b0bd0bb99bc54672bd6cf83524f80f48
Suraj <kumarsuraj54057@gmail.com>
2022-06-12 15:08:28 +0530

    jwt token

--- 4.food-delivery-app/src/user/controllers/user.js
+++ 4.food-delivery-app/src/user/controllers/user.js
@@ -1,6 +1,7 @@
 
 const User = require("../models/user");
 const repo = require("../repositories/userRepo");
+const jwt = require("jsonwebtoken");
 
 // api/user/register
 module.exports.register = async (req, res)=>{
@@ -17,8 +18,25 @@ module.exports.register = async (req, res)=>{
 }
 
 // api/user/login
-module.exports.login = (req, res)=>{
-    // 1. read req.body
-    // 2. db calls.
-    res.end("This is login request");
+module.exports.login = async (req, res)=>{
+
+    // 1. Check if email and password is correct.
+    const email = req.body.email;
+    const user = await repo.getByEmail(email);
+    if(!user || (user.password!=req.body.password)){
+        return res.status(400).send("Invalid Email");
+    }else{
+
+        // 2. Create a jwt (jsonwebtoken).
+        const token = jwt.sign(
+            {userid:user._id},
+            "THISISMYPRIVATEKEY",
+            {
+                expiresIn: "2h"
+            }
+        );
+
+        // 3. Send jwt token in response.
+        return res.status(200).send(token);
+    }
 }
--- 4.food-delivery-app/src/user/repositories/userRepo.js
+++ 4.food-delivery-app/src/user/repositories/userRepo.js
@@ -1,13 +1,24 @@
 
 const mongodbConfig = require("../../../config/mongodb");
 
-module.exports.add = async (user)=>{
+exports.add = async (user)=>{
     const collection = mongodbConfig.getCollection("User");
     try{
-    await collection.insertOne({name: user.name, email: user.email, password: user.password});
+        await collection.insertOne({name: user.name, email: user.email, password: user.password});
     }catch(err){
         console.log(err);
         return false;
     }
     return true;
+}
+
+exports.getByEmail = async (email)=>{
+    const collection = mongodbConfig.getCollection("User");
+    try{
+        const user = await collection.findOne({email});
+        return user;
+    }catch(err){
+        console.log(err);
+        return null;
+    }
 }
